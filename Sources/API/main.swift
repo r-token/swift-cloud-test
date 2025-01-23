62297bdd4e061c02dd5bd10ad48b1f26776db225
Ryan Token <ryantoken13@gmail.com>
2025-01-23 09:43:57 -0700

    Remove unnecessary HelloResponse object

--- Sources/API/main.swift
+++ Sources/API/main.swift
@@ -9,19 +9,10 @@ import AWSLambdaEvents
 import AWSLambdaRuntime
 import Foundation
 
-// the data structure to represent the input parameter
+// the data structure we'll decode out of the API response
 struct HelloRequest: Decodable {
     let name: String
     let age: Int
-}
-
-// the data structure to represent the output response
-struct HelloResponse: Encodable {
-    let statusCode: Int = 200
-    let headers: [String: String] = [
-        "Content-Type": "application/json"
-    ]
-    let body: String
 }
 
 let runtime = LambdaRuntime { (event: APIGatewayV2Request, context: LambdaContext) -> APIGatewayV2Response in
