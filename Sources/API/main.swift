521c0d99f123304d65843582bdbc54e35ff1fe38
Ryan Token <ryantoken13@gmail.com>
2025-01-23 11:21:05 -0700

    Send JSON back as the API response

--- Sources/API/main.swift
+++ Sources/API/main.swift
@@ -15,14 +15,26 @@ struct HelloRequest: Decodable {
     let age: Int
 }
 
+struct HelloResponse: Encodable {
+    let body: HelloBody
+}
+
+struct HelloBody: Encodable {
+    let greeting: String
+    let propertyTwo: Int
+}
+
 let runtime = LambdaRuntime { (event: APIGatewayV2Request, context: LambdaContext) -> APIGatewayV2Response in
     context.logger.info("Received request: \(event)")
-    let body = try JSONDecoder().decode(HelloRequest.self, from: (event.body?.data(using: .utf8))!)
-    let greeting = "Hello \(body.name). You look \(body.age > 30 ? "younger" : "older") than your age."
+    let eventBody = try JSONDecoder().decode(HelloRequest.self, from: (event.body?.data(using: .utf8))!)
+    let greeting = "Hello \(eventBody.name). You look \(eventBody.age > 30 ? "younger" : "older") than your age."
+    let responseBody = HelloResponse(body: HelloBody(greeting: greeting, propertyTwo: 100))
+    let response = try JSONEncoder().encode(responseBody).base64EncodedString()
 
     return APIGatewayV2Response(
         statusCode: .ok,
-        body: greeting
+        body: response,
+        isBase64Encoded: true
     )
 }
 
